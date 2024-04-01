81d407edcea21547bdf1cf3c76e06f3003f53915
Samuell Torres <samuelltorres145@gmail.com>
2024-04-01 20:52:26 -0300

    :sparkles: feat: controller + rota de categorias

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 
 import { baseRoute } from './base.route';
+import { categoriesRoutes } from './categories.route';
 
 export const routes = Router();
 
 routes.use('/', baseRoute);
+routes.use('/categories', categoriesRoutes);
