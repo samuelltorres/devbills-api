84f4e0bb3d54ef8d8e40c92b6fffd06edeab5eb0
Samuell Torres <samuelltorres145@gmail.com>
2024-04-03 14:15:20 -0300

    :sparkles: feat(validacao): schema validation no codigo de cores usando zod + regex

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
+import { z } from 'zod';
 
 import { CategoriesRepository } from '../database/repositories/categories.repository';
 import { CategoryModel } from '../database/schemas/category.schema';
 import { CreateCategoryDTO } from '../dtos/categories.dto';
 import { CategoriesService } from '../services/categories.service';
-
 export class CategoriesController {
   async create(
     req: Request<unknown, unknown, CreateCategoryDTO>,
@@ -12,6 +12,13 @@ export class CategoriesController {
     next: NextFunction,
   ) {
     try {
+      const validateSchema = z.object({
+        title: z.string(),
+        color: z.string().regex(/^#[A-Fa-f0-9]{6}$/),
+      });
+
+      validateSchema.parse(req.body);
+
       const { title, color } = req.body;
 
       const repository = new CategoriesRepository(CategoryModel);
