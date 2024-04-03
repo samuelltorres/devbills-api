d92c1d82aa109d3a7e5d165b29ac03db909843c9
Samuell Torres <samuelltorres145@gmail.com>
2024-04-03 20:44:44 -0300

    :sparkles: feat(Transação): criado o service

---
+++ src/services/transactions.service.ts
@@ -0,0 +1,42 @@
+import { StatusCodes } from 'http-status-codes';
+
+import { CategoriesRepository } from '../database/repositories/categories.repository';
+import { TransactionsRepository } from '../database/repositories/transactions.repository';
+import { CreateTransactionDTO } from '../dtos/transactions.dto';
+import { Transaction } from '../entities/transactions.entity';
+import { AppError } from '../errors/app.error';
+
+export class TransactionsService {
+  constructor(
+    private transactionsRepository: TransactionsRepository,
+    private categoriesRepositoy: CategoriesRepository,
+  ) {}
+
+  async create({
+    title,
+    type,
+    date,
+    categoryId,
+    amount,
+  }: CreateTransactionDTO): Promise<Transaction> {
+    // precisa validar se categoria existe
+    const category = await this.categoriesRepositoy.findById(categoryId);
+
+    if (!category) {
+      throw new AppError('Category does not exists.', StatusCodes.NOT_FOUND);
+    }
+
+    const transaction = new Transaction({
+      title,
+      type,
+      date,
+      category,
+      amount,
+    });
+
+    const createdTransaction =
+      await this.transactionsRepository.create(transaction);
+
+    return createdTransaction;
+  }
+}
