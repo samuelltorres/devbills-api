c0763e10cb97edb288293ef290013046659d7ad5
Samuell Torres <samuelltorres145@gmail.com>
2024-04-03 19:48:59 -0300

    :zap: perf(Factories): criado factory para controlar as requisicoes e nao ter instancias repetidas

--- src/routes/categories.route.ts
+++ src/routes/categories.route.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import { CategoriesController } from '../controllers/categories.controller';
 import { createCategorySchema } from '../dtos/categories.dto';
+import { CategoriesFactory } from '../factories/categories.factory';
 import { ParamsType, validator } from '../middlewares/validator.middleware';
 
 export const categoriesRoutes = Router();
 
-const controller = new CategoriesController();
+const controller = new CategoriesController(
+  CategoriesFactory.getServiceInstance(),
+);
 
 categoriesRoutes.get('/', controller.index);
 categoriesRoutes.post(
