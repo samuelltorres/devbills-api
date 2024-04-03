e28f9811796a7a3c930250aa4e07339736a1a5fc
Samuell Torres <samuelltorres145@gmail.com>
2024-04-03 20:43:30 -0300

    :pencil: fix(Transação): correção de ortografia

--- src/dtos/transactions.dto.ts
+++ src/dtos/transactions.dto.ts
@@ -6,7 +6,7 @@ export const createTransactionsSchema = {
   title: z.string(),
   amount: z.number().int().positive(),
   type: z.nativeEnum(TransactionType),
-  data: z.coerce.date(),
+  date: z.coerce.date(),
   categoryId: z.string(),
 };
 
