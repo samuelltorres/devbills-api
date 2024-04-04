0adde37c19e04d719bd0ddebc1d119c18cea8dfb
Samuell Torres <samuelltorres145@gmail.com>
2024-04-03 21:46:32 -0300

    :sparkles: feat(Transação - DTO): dto para configuração dos filtros de transacoes

--- src/dtos/transactions.dto.ts
+++ src/dtos/transactions.dto.ts
@@ -12,3 +12,13 @@ export const createTransactionsSchema = {
 
 const createTransactionoObject = z.object(createTransactionsSchema);
 export type CreateTransactionDTO = z.infer<typeof createTransactionoObject>;
+
+export const indexTransactionsSchema = {
+  title: z.string().optional(),
+  categoryId: z.string().length(24).optional(),
+  beginDate: z.coerce.date().optional(),
+  endDate: z.coerce.date().optional(),
+};
+
+const indexTransactionsObject = z.object(indexTransactionsSchema);
+export type IndexTransactionsDTO = z.infer<typeof indexTransactionsObject>;
