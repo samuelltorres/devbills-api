fd9eb4241dd6f65c2c4bd75404320d5c38816709
Samuell Torres <samuelltorres145@gmail.com>
2024-04-03 21:15:20 -0300

    :sparkles: feat(Transação): criado a rota para transacoes

---
+++ src/routes/transactions.route.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import { TransactionsController } from '../controllers/transactions.controller';
+import { createTransactionsSchema } from '../dtos/transactions.dto';
+import { TransactionsFactory } from '../factories/transactions.factory';
+import { ParamsType, validator } from '../middlewares/validator.middleware';
+
+export const transactionsRoutes = Router();
+
+const controller = new TransactionsController(
+  TransactionsFactory.getServiceInstance(),
+);
+
+transactionsRoutes.post(
+  '/',
+  validator({
+    schema: createTransactionsSchema,
+    type: ParamsType.BODY,
+  }),
+  controller.create,
+);
